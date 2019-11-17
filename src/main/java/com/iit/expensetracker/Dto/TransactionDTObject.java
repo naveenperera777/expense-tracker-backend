93b7ee016ee1219548f28c29766a372e5e412946
Naveen <naveen@arimaclanka.com>
2019-11-17 19:04:44 +0530

    Change transaction dto

--- src/main/java/com/iit/expensetracker/Dto/TransactionDTObject.java
+++ src/main/java/com/iit/expensetracker/Dto/TransactionDTObject.java
@@ -1,32 +1,37 @@
 package com.iit.expensetracker.Dto;
 
 public class TransactionDTObject {
-    private String categoryId;
-    private double amount;
-    private String remarks;
 
-    public String getCategoryId() {
-        return categoryId;
+    private String category_id;
+    private double transaction_amount;
+    private String transaction_notes;
+
+
+    public String getCategory_id() {
+        return category_id;
     }
 
-    public void setCategoryId(String categoryId) {
-        this.categoryId = categoryId;
+    public void setCategory_id(String category_id) {
+        this.category_id = category_id;
     }
 
-    public double getAmount() {
-        return amount;
+    public double getTransaction_amount() {
+        return transaction_amount;
     }
 
-    public void setAmount(double amount) {
-        this.amount = amount;
+    public void setTransaction_amount(double transaction_amount) {
+        this.transaction_amount = transaction_amount;
     }
 
-    public String getRemarks() {
-        return remarks;
+    public String getTransaction_notes() {
+        return transaction_notes;
     }
 
-    public void setRemarks(String remarks) {
-        this.remarks = remarks;
+    public void setTransaction_notes(String transaction_notes) {
+        this.transaction_notes = transaction_notes;
     }
+
+
+
 
 }
