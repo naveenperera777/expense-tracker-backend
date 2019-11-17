b917abc117e6d5f16e05242b14e72489c9e354d9
Naveen <naveen@arimaclanka.com>
2019-11-17 22:06:33 +0530

    Add percentage to response

--- src/main/java/com/iit/expensetracker/Service/Impl/ReportServiceImpl.java
+++ src/main/java/com/iit/expensetracker/Service/Impl/ReportServiceImpl.java
@@ -1,12 +1,22 @@
 package com.iit.expensetracker.Service.Impl;
 
 import com.iit.expensetracker.DAO.ReportDAO;
+import com.iit.expensetracker.Dto.CategoryLimitResponseDto;
+import com.iit.expensetracker.Dto.CategoryPercentageDto;
+import com.iit.expensetracker.Response.Response;
 import com.iit.expensetracker.Service.ReportService;
+import com.iit.expensetracker.enums.ResponseMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ReportServiceImpl implements ReportService {
     private final ReportDAO reportDAO;
+    private static final Logger logger = LoggerFactory.getLogger(ReportServiceImpl.class);
 
     public ReportServiceImpl(ReportDAO reportDAO) {
         this.reportDAO = reportDAO;
@@ -14,6 +24,30 @@ public class ReportServiceImpl implements ReportService {
 
     @Override
     public Object getAllCategoriesWithExpensesAndLimit(String userId, String month) {
-        return reportDAO.getAllCategoriesWithExpensesAndLimit(userId, month);
+        List<CategoryLimitResponseDto> categoryList = reportDAO.getAllCategoriesWithExpensesAndLimit(userId, month);
+        if (categoryList.isEmpty())
+            return new Response(ResponseMessage.NO_RECORD, "No records found");
+        List<CategoryPercentageDto> listWithPercentage = new ArrayList<>();
+        for (CategoryLimitResponseDto category : categoryList) {
+            CategoryPercentageDto categoryPercentageDto = new CategoryPercentageDto();
+            categoryPercentageDto.setUserId(category.getUserId());
+            categoryPercentageDto.setCategoryId(category.getCategoryId());
+            categoryPercentageDto.setCategory(category.getCategory());
+            categoryPercentageDto.setType(category.getType());
+            categoryPercentageDto.setLimit(category.getLimit());
+            categoryPercentageDto.setTotalExpenes(category.getTotalExpenes());
+
+            if (category.getTotalExpenes()<category.getLimit() || category.getTotalExpenes() == category.getLimit()){
+               double percentage = (category.getTotalExpenes()/category.getLimit())*100;
+                categoryPercentageDto.setExceeded(false);
+                categoryPercentageDto.setPercentage((int)percentage);
+            } else {
+                double percentage = (category.getLimit()/category.getTotalExpenes())*100;
+                categoryPercentageDto.setExceeded(true);
+                categoryPercentageDto.setPercentage((int)percentage);
+            }
+            listWithPercentage.add(categoryPercentageDto);
+        }
+        return new Response(ResponseMessage.SUCCESS, listWithPercentage);
     }
 }
